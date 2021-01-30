9a1e6f89d96f4119f4d25a939d45cf48e6b201b3
Laura Baltrukonė <baltrukone@gmail.com>
2021-01-30 14:10:41 +0200

    added description fields in data

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,22 +10,26 @@ class Pages extends Controller
         // echo 'hello from pages controller';
     }
 
+    // ======================================INDEX===========================================
     public function index()
     {
         //create some data to load into view
         $data = [
             'title' => 'Welcome to ' . SITENAME,
+            'description'=>'This is an app to share your Thoughts with the World'
         ];
         //load the view
         $this->view('pages/index', $data);
     }
 
+    // ======================================ABOUT===========================================
     //padarom nauja puslapi
     public function about()
     {
         //create some data to load into view
         $data = [
-            'title' => 'Welcome to About page- ' . SITENAME,
+            'title' => 'About - ' . SITENAME,
+            'description'=>'App to share news with friends and World'
         ];
         //load the view
         $this->view('pages/about', $data);
