a49a051a54d8c2e122aafe6cdd263d008baa05ba
Laura Baltrukonė <baltrukone@gmail.com>
2021-01-30 15:54:41 +0200

    register form validation and redirect to die

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -20,6 +20,62 @@ class Users extends Controller
         //echo 'Register in progress';
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // form process in progress
+
+            //sanitize Post Array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            //create data
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirmPassword' => trim($_POST['confirmPassword']),
+                'nameErr' => '',
+                'emailErr' => '',
+                'passwordErr' => '',
+                'confirmPasswordErr' => '',
+            ];
+
+            // Validate name
+            if (empty($data['name'])) {
+                // empty field
+                $data['nameErr'] = 'Please enter your Name';
+            }
+            // Validate email
+            if (empty($data['email'])) {
+                // empty field
+                $data['emailErr'] = 'Please enter your Email';
+            } else {
+                if (filter_var($data['email'], FILTER_VALIDATE_EMAIL) === false) {
+                    $data['emailErr'] = 'Please check Your Email';
+                }
+            }
+            // Validate password
+            if (empty($data['password'])) {
+                // empty field
+                $data['passwordErr'] = 'Please enter your Password';
+            } elseif (strlen($data['password']) < 4) {
+                $data['passwordErr'] = 'Password must be 4 or more characters';
+            }
+            // Validate confirmPassword
+            if (empty($data['confirmPassword'])) {
+                // empty field
+                $data['confirmPasswordErr'] = 'Please repeat Password';
+            } else {
+                if ($data['confirmPassword'] !== $data['password']) {
+                    $data['confirmPasswordErr'] = 'Password must match';
+                }
+            }
+
+            //if there is no errors
+            if (empty($data['nameErr']) && empty($data['emailErr']) && empty($data['passwordErr']) && empty(['confirmPasswordErr'])) {
+                //there ar no errors
+                var_dump('labas');
+                die('SUCCESS');
+            } else {
+                //load view with errors
+                $this->view('users/register', $data);
+            }
         } else {
             //load form
 //            echo 'load form';
