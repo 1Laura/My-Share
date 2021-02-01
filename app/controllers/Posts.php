c3764c0d36e30deedb3bf9ff7d41c64b500a9164
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-01 09:30:57 +0200

    restrict access

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -8,10 +8,16 @@
 
 class Posts extends Controller
 {
+    public function __construct()
+    {
+        //restrict access of this controller only logged in users
+        if (!isLoggedIn()) redirect('/users/login');
+    }
 
     public function index()
     {
-        $data=[];
+
+        $data = [];
         $this->view('posts/index', $data);
 
     }
