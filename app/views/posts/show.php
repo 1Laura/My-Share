53cad02a7b497d1918f2fd517062dc7f56b20cbd
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-02 09:02:05 +0200

    single post view in progress

---
+++ app/views/posts/show.php
@@ -0,0 +1,9 @@
+<?php require APPROOT . '\views\includes\head.php'; ?>
+
+<h1>Single page template</h1>
+
+
+<?php  echo var_dump($data['postId']) ?>
+
+<?php require APPROOT . '\views\includes\footer.php'; ?>
+
