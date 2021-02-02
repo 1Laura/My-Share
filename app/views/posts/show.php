33068f11bcf8c3b2bcd4161fef67e1356490ce6c
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-02 10:07:37 +0200

    add userModel to posts and getUserById

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -1,10 +1,17 @@
 <?php require APPROOT . '\views\includes\head.php'; ?>
 
-<h1>Single page template</h1>
 
-<?php var_dump($data['post']->id) ?>
-<?php var_dump($data['post']) ?>
+<a href="<?php echo URLROOT ?>/posts" class="btn btn-light my-3"><i class="fa fa-chevron-left"></i> Back</a>
+<h1><?php echo $data['post']->title; ?></h1>
+<div class="bg-secondary text-white p-2 mb-3">
+    Written by: <strong><?php echo $data['user']->name; ?></strong>>
+    On: <?php echo $data['post']->created; ?>
+</div>
+<p class="lead"><?php echo $data['post']->body; ?></p>
 
 
-<?php require APPROOT . '\views\includes\footer.php'; ?>
+<?php
+//var_dump($data['post']);
+//var_dump($data['user']);
+require APPROOT . '\views\includes\footer.php'; ?>
 
