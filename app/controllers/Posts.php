6780b28398cb605ea81a1c6109ac2fb1f8958248
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-02 10:58:15 +0200

    edit method with validation and correct data flow

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -9,6 +9,7 @@
 class Posts extends Controller
 {
     private $postModel;
+    private $userModel;
 
     public function __construct()
     {
@@ -16,6 +17,7 @@ class Posts extends Controller
         if (!isLoggedIn()) redirect('/users/login');
 
         $this->postModel = $this->model('Post');
+        $this->userModel = $this->model('User');
     }
 
     public function index()
@@ -90,13 +92,85 @@ class Posts extends Controller
 
         //get post row
         $post = $this->postModel->getPostById($id);
+
+        //lets get user data by userId
+        $user = $this->userModel->getUserById($post->userId);
+
         //create data for the view and add post data
         $data = [
-            'post' => $post
+            'post' => $post,
+            'user' => $user
         ];
         //load view
         $this->view('posts/show', $data);
 
 
     }
+
+
+    public function edit($id = null)
+    {
+        if ($id === null) redirect('/posts');
+        //lets validate
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //lets validate
+            //issivalom posto masyva
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'userId' => $_SESSION['userId'],
+                'title' => trim($_POST['title']),
+                'body' => trim($_POST['body']),
+                'titleErr' => '',
+                'bodyErr' => ''
+            ];
+
+            //validate title
+            if (empty($data['title'])) {
+                $data['titleErr'] = 'Please enter a title';
+            }
+            //validate body
+            if (empty($data['body'])) {
+                $data['bodyErr'] = 'Please enter a text';
+            }
+
+            //check if there are no errors
+            if (empty($data['titleErr']) && empty($data['bodyErr'])) {
+                // there ar no errors
+                // die('no errors, can submit');
+                if ($this->postModel->updatePost($data)) {
+                    //post added
+                    flash('postMessage', 'You have edited the post');
+                    redirect('/posts');
+                } else {
+                    die('something went wrong in editing post');
+                }
+            } else {
+                //load view with errors
+                $this->view('posts/edit', $data);
+            }
+        } else {
+            //else user entered onto this page
+            $post = $this->postModel->getPostById($id);
+
+            if ($post) {
+                //check if this posts belong to this user
+                if ($post->userId !== $_SESSION['userId']) redirect('/posts');
+
+            } else {
+                die('something wnt wrong getPostById');
+            }
+
+            //post found and will load view
+            $data = [
+                'id'=>$id,
+                'title' => $post->title,
+                'body' => $post->body,
+                'titleErr' => '',
+                'bodyErr' => ''
+            ];
+            $this->view('posts/edit', $data);
+        }
+    }
+
 }
