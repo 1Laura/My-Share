c8f8e39b6d8082b9ef2b4447954d94fca11a9f16
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-02 10:42:32 +0200

    show edit and delete only if post belongs to current user

--- app/views/posts/show.php
+++ app/views/posts/show.php
@@ -4,14 +4,25 @@
 <a href="<?php echo URLROOT ?>/posts" class="btn btn-light my-3"><i class="fa fa-chevron-left"></i> Back</a>
 <h1><?php echo $data['post']->title; ?></h1>
 <div class="bg-secondary text-white p-2 mb-3">
-    Written by: <strong><?php echo $data['user']->name; ?></strong>>
+    Written by: <strong><?php echo $data['user']->name; ?></strong>
     On: <?php echo $data['post']->created; ?>
 </div>
 <p class="lead"><?php echo $data['post']->body; ?></p>
 
 
+<!--show this only if this post belongs to this user-->
+<?php if ($_SESSION['userId'] === $data['post']->userId) : ?>
+    <hr>
+    <a href="<?php echo URLROOT . "/posts/edit/" . $data['post']->id; ?>" class="btn btn-secondary"><i
+                class="fa fa-pencil"></i> EDIT </a>
+
+    <form action="<?php echo URLROOT ?>/posts/delete" method="post" class="pull-right">
+        <button type="submit" class="btn btn-danger"><i class="fa fa-close"></i> DELETE</button>
+    </form>
+<?php endif; ?>
 <?php
 //var_dump($data['post']);
 //var_dump($data['user']);
+//var_dump($_SESSION);
 require APPROOT . '\views\includes\footer.php'; ?>
 
