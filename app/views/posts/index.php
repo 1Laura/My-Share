90c23c994607aae9246b8d9e8e81ffe2b01c8857
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-02 09:25:17 +0200

    insert flash FN

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -3,6 +3,7 @@
 <div class="row mt-2">
     <div class="col">
         <h1>Posts</h1>
+        <?php echo flash('postMessage'); ?>
     </div>
     <div class="col">
         <a href="<?php echo URLROOT; ?>/posts/add" class="btn btn-secondary pull-right"><i class="fa fa-pencil"></i> Add
@@ -15,13 +16,14 @@
             <div class="card border-secondary mb-3">
                 <div class="card-header border-secondary">Written by: <?php echo $post->name ?></div>
                 <div class="card card-body">
-                    <h4 class="card-title"><?php echo $post->title ?>></h4>
+                    <h4 class="card-title"><?php echo $post->title ?></h4>
                     <!--            <p class="bg-light p-2 mb-3">Written by USER</p>-->
                     <p class="card-text"><?php echo $post->body ?></p>
+                    <a href="<?php echo URLROOT . '/posts/show/' . $post->postId; ?>"
+                       class="card-link text-decoration-none">Read More</a>
                     <!--                    <a href="-->
-                    <?php //echo URLROOT . '/posts/show/' . $post->postId; ?><!--" class="card-link">Read More</a>-->
-                    <a href="<?php echo URLROOT . '/posts/show/' . $post->postId; ?>" class="btn btn-xs btn-outline-secondary">Read
-                        More</a>
+                    <?php //echo URLROOT . '/posts/show/' . $post->postId; ?><!--" class="btn btn-outline-secondary">Read-->
+                    <!--                        More</a>-->
                 </div>
                 <div class="card-footer border-secondary">Created
                     at: <?php echo $post->postCreated ?></div>
