e4cf4e0087217213a785be4889ba59dcf9d725c4
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-03 08:59:09 +0200

    added new class Validation in libraries

---
+++ app/libraries/Validation.php
@@ -0,0 +1,15 @@
+<?php
+//this is a class to validate  different inputs and data
+
+class Validation
+{
+
+    //checks if server request is post
+    public function ifRequestIsPost(): bool
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') return true;
+        return false;
+    }
+
+
+}
