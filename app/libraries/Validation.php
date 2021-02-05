f5155afb2a27675b1f2255afc5cf34a2967a8735
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-05 09:11:06 +0200

    if emprty field methods with and without references

--- app/libraries/Validation.php
+++ app/libraries/Validation.php
@@ -26,5 +26,42 @@ class Validation
         return false;
     }
 
+    public function ifEmptyErrorsArray($errorsArr)
+    {
+//       (empty($data['errors']['nameErr']) && empty($data['errors']['emailErr']) && empty($data['errors']['passwordErr']) && empty($data['errors']['confirmPasswordErr']))
+        //foreach
+        // check if all values of array is empty`1
+        foreach ($errorsArr as $error) {
+            if (!empty($error)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // dvi funkcijos kaip galima pasitikrinti
+    //funkcija su referencu
+    public function ifEmptyUserFieldWithReference(&$data, $field, $fieldDisplayName)
+    {
+        $fieldError = $field . "Err";
+        if (empty($data[$field])) {
+            // empty field
+            $data['errors'][$fieldError] = "Please enter your $fieldDisplayName";
+        }
+
+    }
+
+    //funkcija be referenco
+    public function ifEmptyUserField($field, $fieldDisplayName, $msg = null): string
+    {
+        if (empty($field)) {
+            // empty field
+            if ($msg) {
+                return $msg;
+            }
+            return "Please enter your $fieldDisplayName";
+        }
+        return '';//falsy
+    }
 
 }
