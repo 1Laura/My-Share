594df74ad867c2f203133f2fb2dd24596f24a621
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-07 00:01:53 +0200

    api controller start

---
+++ app/controllers/API.php
@@ -0,0 +1,28 @@
+<?php
+
+//class for returning json formatted data
+
+class API extends Controller
+{
+    private $commentModel;
+
+    public function __construct()
+    {
+        $this->commentModel = $this->model('Comment');
+    }
+
+    public function index()
+    {
+        echo 'index in api';
+    }
+
+
+    public function comments()
+    {
+        $data = [
+            'id' => 'comments'
+        ];
+        echo json_encode($data);
+
+    }
+}
