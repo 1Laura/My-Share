42c639428c89cb5f3612bb9258f47a53e283952f
Laura Baltrukonė <baltrukone@gmail.com>
2021-02-12 09:25:57 +0200

    added script

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -9,32 +9,24 @@
             <form action="" method="post">
                 <div class="form-group">
                     <label for="name">Name: <sup>*</sup></label>
-                    <input type="text" name="name" id="name"
-                           class="<?php echo !empty($data['errors']['nameErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg"
-                           value="<?php echo $data['name']; ?>">
+                    <input type="text" name="name" id="name" class="<?php echo !empty($data['errors']['nameErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['name']; ?>">
                     <span class="invalid-feedback"><?php echo $data['errors']['nameErr']; ?></span>
                 </div>
 
                 <div class="form-group">
                     <label for="email">Email: <sup>*</sup></label>
-                    <input type="text" name="email" id="email"
-                           class="<?php echo !empty($data['errors']['emailErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg"
-                           value="<?php echo $data['email']; ?>">
+                    <input type="text" name="email" id="email" class="<?php echo !empty($data['errors']['emailErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['email']; ?>">
                     <span class="invalid-feedback"><?php echo $data['errors']['emailErr']; ?></span>
                 </div>
 
                 <div class="form-group">
                     <label for="password">Password: <sup>*</sup></label>
-                    <input type="password" name="password" id="password"
-                           class="<?php echo !empty($data['errors']['passwordErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg"
-                           value="<?php echo $data['password']; ?>">
+                    <input type="password" name="password" id="password" class="<?php echo !empty($data['errors']['passwordErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['password']; ?>">
                     <span class="invalid-feedback"><?php echo $data['errors']['passwordErr']; ?></span>
                 </div>
                 <div class="form-group">
                     <label for="confirmPassword">Confirm password: <sup>*</sup></label>
-                    <input type="password" name="confirmPassword" id="confirmPassword"
-                           class="<?php echo !empty($data['errors']['confirmPasswordErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg"
-                           value="<?php echo $data['confirmPassword']; ?>">
+                    <input type="password" name="confirmPassword" id="confirmPassword" class="<?php echo !empty($data['errors']['confirmPasswordErr']) ? 'is-invalid' : ''; ?> form-control form-control-lg" value="<?php echo $data['confirmPassword']; ?>">
                     <span class="invalid-feedback"><?php echo $data['errors']['confirmPasswordErr']; ?></span>
                 </div>
 
@@ -52,6 +44,10 @@
         </div>
     </div>
 </div>
+
+<script>
+console.log('labas');
+</script>
 
 
 <?php require APPROOT . '\views\includes\footer.php'; ?>
